631333f71ed9c2d84eb97da795a9b544f7c5f723
CashewRose <danieagnoletti@gmail.com>
2018-04-06 11:47:35 -0500

    Initial commit started on practice

---
+++ main.js
@@ -0,0 +1,51 @@
+let elizabeth = {
+    distract: "14 of HungerGames",
+    statements:[
+        {
+        taxes: "Less taxes for the poor, more for the athletes",
+        jobs: "we should make those",
+        infrastructure: "More schools and teleporters",
+        healthCare: "Available to all! More vacation days to prevent illness",
+        crimeEnforcement: "We should prevent crime! Lets make that a thing..."
+        }
+    ],
+    donationURL: "https://www.givememoney.com",
+    calender: [
+        { 
+            january: "15",
+            febuary: "10",
+            march: "11",
+            april: "25",
+            may: "1",
+            june: "17",
+            july: "9",
+            august: "21",
+            september: "8",
+            october: "31",
+            november: "3",
+            december: "27"
+        }
+    ],
+    volunteer: [
+        {
+            name: "",
+            address: "",
+            email: "",
+            phoneNum: "",
+            availability: "",
+            position: ""
+        }
+    ],
+    biography: "I like to improve things! Thats why we decided to travel dimensions into the geek relm we are today! Help me do that by providing all the memes!",
+    photos: [
+        {
+           head: "https://cdn.pixabay.com/photo/2018/02/01/20/48/woman-3124083_1280.jpg",
+           family: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Doctor_Who_Exhibition_%283568915919%29.jpg",
+           enemies: "https://c1.staticflickr.com/8/7563/16079238881_6e3dccef18_z.jpg%22", 
+        }
+    ],
+    mission: "To better ourselves as a growing technilogical community. We should unite and geekify like the old earth nerd conventions! I will strive to accomplish just that!",
+    register: ""
+}
+
+alert(elizabeth.statements[0].taxes);
